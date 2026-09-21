b140eeccf9dc3bcdacf118cc3ad36513786f6d0b
vivekanand8851-bit <vivekanand8851@gmail.com>
2026-09-21 17:41:30 +0530

    Add herbal SEO landing page routes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import WishlistPage from './pages/WishlistPage';
 import AdminPage from './pages/AdminPage';
 import { AboutPage, ContactPage, ConsultPage, LoginPage, AccountPage, TrackOrderPage, PoliciesPage } from './pages/StaticPages';
 import { BlogListPage, BlogDetailPage } from './pages/BlogPage';
+import HerbalLandingPage from './pages/HerbalLandingPage';
 
 export default function App() {
   return (
@@ -24,6 +25,9 @@ export default function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/shop" element={<ShopPage />} />
+            <Route path="/ayurvedic" element={<HerbalLandingPage type="ashwagandha" />} />
+            <Route path="/ayurvedic/ashwagandha" element={<HerbalLandingPage type="ashwagandha" />} />
+            <Route path="/ayurvedic/shilajit" element={<HerbalLandingPage type="shilajit" />} />
             <Route path="/product/:slug" element={<ProductDetailPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
